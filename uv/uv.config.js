03221e0b9a8fcd0963d8c6ba3b2c17f8f8bee07f
bruh <89742348+sauc23@users.noreply.github.com>
2024-12-03 11:03:02 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://in.omada.bet/fq/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
